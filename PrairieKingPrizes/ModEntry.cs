fc873e152dc70ffd2f31133fca4da67ab273c48f
Renny <renaldas.sorys@gmail.com>
2018-09-12 16:49:44 +0100

    Pre-cleanup commit

--- PrairieKingPrizes/ModEntry.cs
+++ PrairieKingPrizes/ModEntry.cs
@@ -27,10 +27,13 @@ namespace PrairieKingPrizes
             GameEvents.UpdateTick += GameEvents_UpdateTick;
             SaveEvents.AfterLoad += AfterSaveLoaded;
             helper.ConsoleCommands.Add("gettokens", "Retrieves the value of your current amount of tokens.", this.GetCoins);
-            SaveEvents.BeforeSave += DeleteNPC_BeforeSave;
-            SaveEvents.AfterSave += AddNPC_AfterSave;
+            helper.ConsoleCommands.Add("settokens", "Sets tokens", this.SetCoins);
+            //SaveEvents.BeforeSave += DeleteNPC_BeforeSave;
+            //SaveEvents.AfterSave += AddNPC_AfterSave;
             InputEvents.ButtonPressed += CheckAction;
         }
+
+
 
         public bool CanEdit<T>(IAssetInfo asset)
         {
@@ -46,32 +49,32 @@ namespace PrairieKingPrizes
             return false;
         }
 
-        public void Edit<T>(IAssetData asset)
-        {
+        //public void Edit<T>(IAssetData asset)
+        // {
         //    if (asset.AssetNameEquals("Data/NPCDispositions"))
         //    {
-        //        asset.AsDictionary<string, string>().Data["TokenMachine"] = "adult/shy/outgoing/negative/male/non-datable/null/Town/fall 9/null/Saloon 34 17/Token Machine";
-        //    }
-
+        //         asset.AsDictionary<string, string>().Data["TokenMachine"] = "adult/shy/outgoing/negative/male/non-datable/null/Town/fall 9/null/Saloon -1000 -1000/Token Machine";
+        //     }
+        //
         //    if (asset.AssetNameEquals("Data/NPCGiftTastes"))
         //    {
         //        IDictionary<string, string> NPCGiftTastes = asset.AsDictionary<string, string>().Data;
         //        NPCGiftTastes["TokenMachine"] = "ERROR: I DO NOT ACCEPT GIFTS//ERROR: I DO NOT ACCEPT GIFTS//ERROR: I DO NOT ACCEPT GIFTS//ERROR: I DO NOT ACCEPT GIFTS/-2 -4 -5 -6 -7 -8 -9 -12 -14 -15 -16 -17 -18 -19 -20 -21 -22 -23 -24 -25 -26 -27 -28 -29 -74 -75 -79 -80 -81 -95 -96 -98 -99/ERROR: I DO NOT ACCEPT GIFTS//";
         //    }
 
-            if (asset.AssetNameEquals("Characters/Dialogue/rainy"))
-            {
-                IDictionary<string, string> rainy = asset.AsDictionary<string, string>().Data;
-                rainy["TokenMachine"] = "I HOPE I DO NOT GET WET IN THIS RAIN. OH WAIT, I CANNOT MOVE. HA. HA.";
-            }
-        }
+        //    if (asset.AssetNameEquals("Characters/Dialogue/rainy"))
+        //   {
+        //      IDictionary<string, string> rainy = asset.AsDictionary<string, string>().Data;
+        //      rainy["TokenMachine"] = "I HOPE I DO NOT GET WET IN THIS RAIN. OH WAIT, I CANNOT MOVE. HA. HA.";
+        //   }
+        //  }
 
-        public bool CanLoad<T>(IAssetInfo asset)
-        {
-                if (asset.AssetNameEquals("Characters/Dialogue/TokenMachine"))
-                {
-                    return true;
-                }
+        // public bool CanLoad<T>(IAssetInfo asset)
+        // {
+        //       if (asset.AssetNameEquals("Characters/Dialogue/TokenMachine"))
+        //       {
+        //           return true;
+        //       }
 
         //        if (asset.AssetNameEquals("Characters/TokenMachine"))
         //        {
@@ -88,15 +91,15 @@ namespace PrairieKingPrizes
         //            return true;
         //        }
 
-            return false;
-        }
+        //   return false;
+        //}
 
-        public T Load<T>(IAssetInfo asset)
-        {
-                if (asset.AssetNameEquals("Characters/Dialogue/TokenMachine"))
-                {
-                    return Helper.Content.Load<T>("assets/tokenDialogue.xnb", ContentSource.ModFolder);
-                }
+        //public T Load<T>(IAssetInfo asset)
+        //{
+        //        if (asset.AssetNameEquals("Characters/Dialogue/TokenMachine"))
+        //        {
+        //            return Helper.Content.Load<T>("assets/tokenDialogue.xnb", ContentSource.ModFolder);
+        //        }
 
         //        if (asset.AssetNameEquals("Characters/Schedules/TokenMachine"))
         //        {
@@ -113,12 +116,28 @@ namespace PrairieKingPrizes
         //            return Helper.Content.Load<T>("assets/portrait.png", ContentSource.ModFolder);
         //        }
 
-            throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
-        }
+        //  throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
+        //}
 
         private void GetCoins(string command, string[] args)
         {
             this.Monitor.Log($"You currently have {totalTokens} coins.");
+        }
+
+        public void SetCoins(string command, string[] args)
+        {
+            int tokensToAdd = 0;
+            if( args.Length > 1)
+            {
+                this.Monitor.Log("Please input only a single number as an argument.");
+                return;
+            }
+            if (Int32.TryParse(args[0], out tokensToAdd))
+            {
+                totalTokens += tokensToAdd;
+                this.Monitor.Log($"{tokensToAdd} tokens added.");
+                tokensToAdd = 0;
+            } else { this.Monitor.Log("NaN!"); }
         }
 
         private void AfterSaveLoaded(object sender, EventArgs args)
@@ -149,43 +168,44 @@ namespace PrairieKingPrizes
             layerBuildings.Tiles[34, 17] = new StaticTile(layerBuildings, customTileSheet, BlendMode.Alpha, 1);
             layerBack.Tiles[34, 18] = new StaticTile(layerBack, customTileSheet, BlendMode.Alpha, 2);
 
-            location.setTileProperty(34, 17, "Buildings", "Action", "TokenNPCDialogue");
+            location.setTileProperty(34, 17, "Buildings", "Action", "TokenMachine");
 
             Texture2D portrait = Helper.Content.Load<Texture2D>("assets/portrait.png", ContentSource.ModFolder);
 
-            if (TokenNPC == null)
-            {
-                this.TokenNPC = new NPC(null, new Vector2(-1000f, -1000f), "Saloon", 3, "TokenMachine", false, null, Helper.Content.Load<Texture2D>("assets/portrait.png", ContentSource.ModFolder));
-                isNPCLoaded = true;
-            }
+            //if (this.TokenNPC == null)
+            //{
+            //    this.TokenNPC = new NPC(null, new Vector2(-1000f, -1000f), "Saloon", 3, "TokenMachine", false, null, Helper.Content.Load<Texture2D>("assets/portrait.png", ContentSource.ModFolder));
+            //    Game1.locations[Game1.locations.Count - 1].addCharacter(this.TokenNPC);
+            //    isNPCLoaded = true;
+            //}
 
-            int basicItemID = 444;
-            int premiumItemID = 445;
-            int cancelID = 446;
+            //int basicItemID = 444;
+            //int premiumItemID = 445;
+            //int cancelID = 446;
 
-            foreach (int num in Game1.player.dialogueQuestionsAnswered)
-            {
-                if (num == basicItemID)
-                {
-                    Game1.player.dialogueQuestionsAnswered.Remove(basicItemID);
-                    this.Monitor.Log($"{basicItemID}/basicItemID was a valid dialogue answer and has been removed.");
-                }
-                if (num == premiumItemID)
-                {
-                    Game1.player.dialogueQuestionsAnswered.Remove(premiumItemID);
-                    this.Monitor.Log($"{premiumItemID}/premiumItemID was a valid dialogue answer and has been removed.");
-                }
-                if (num == cancelID)
-                {
-                    Game1.player.dialogueQuestionsAnswered.Remove(cancelID);
-                    this.Monitor.Log($"{cancelID}/cancelID was a valid dialogue answer and has been removed.");
-                }
-            }
+            //foreach (int num in game1.player.dialoguequestionsanswered)
+            //{
+            //    if (num == basicitemid)
+            //    {
+            //        game1.player.dialoguequestionsanswered.remove(basicitemid);
+            //        this.monitor.log($"{basicitemid}/basicitemid was a valid dialogue answer and has been removed.");
+            //    }
+            //    if (num == premiumitemid)
+            //    {
+            //        game1.player.dialoguequestionsanswered.remove(premiumitemid);
+            //        this.monitor.log($"{premiumitemid}/premiumitemid was a valid dialogue answer and has been removed.");
+            //    }
+            //    if (num == cancelid)
+            //    {
+            //        game1.player.dialoguequestionsanswered.remove(cancelid);
+            //        this.monitor.log($"{cancelid}/cancelid was a valid dialogue answer and has been removed.");
+            //    }
+            //}
         }
 
         private void CheckAction(object sender, EventArgsInput e)
         {
-            if (Context.IsPlayerFree && e.IsActionButton && TokenNPC != null)
+            if (Context.IsPlayerFree && e.IsActionButton)
             {
                 Vector2 grabTile = new Vector2((float)(Game1.getOldMouseX() + Game1.viewport.X), (float)(Game1.getOldMouseY() + Game1.viewport.Y)) / (float)Game1.tileSize;
                 if (!Utility.tileWithinRadiusOfPlayer((int)grabTile.X, (int)grabTile.Y, 1, Game1.player))
@@ -200,36 +220,58 @@ namespace PrairieKingPrizes
                 }
                 if (propertyValue != null)
                 {
-                    if (propertyValue == "TokenNPCDialogue")
+                    if (propertyValue == "TokenMachine")
                     {
-                        this.Monitor.Log("Player has clicked on the Token Machine");
-                        Game1.drawDialogue(TokenNPC);
+                        //this.Monitor.Log("Player has clicked on the Token Machine");
+                        //NPC TokenMachine = Game1.getCharacterFromName("TokenMachine");
+                        //Game1.drawDialogue(TokenMachine);
+
+                        Response basic = new Response("Basic", "Basic Tier (10 Tokens)");
+                        Response premium = new Response("Premium", "Premium Tier (50 Tokens)");
+                        Response cancel = new Response("Cancel", "Cancel");
+                        Response[] answers = { basic, premium, cancel };
+
+                        
+                        Game1.player.currentLocation.createQuestionDialogue("Would you like to spend your tokens to receive a random item?", answers, AfterQuestion, null);
+                        
                     }
                 }
             }
         }
 
-        private void DeleteNPC_BeforeSave(object sender, EventArgs args)
+        private void AfterQuestion(Farmer who, string whichAnswer)
         {
-            if (!Context.IsWorldReady) { return; }
-
-            //if (isNPCLoaded == true)
-            //{
-            //    Game1.removeThisCharacterFromAllLocations(tokenNPC);
-            //    isNPCLoaded = false;
-            //    foreach (GameLocation location in Game1.locations)
-            //    {
-            //        NPC[] matches = location.characters.Where(p => p.Name == "TokenMachine").ToArray();
-            //        if (matches.Any())
-            //            Monitor.Log($"Found the NPC in {location.Name}.", LogLevel.Warn);
-
-            //    }
-            //}
-            //else { return; }
+            this.Monitor.Log("Successfully called the AfterQuestion method");
+            if (whichAnswer == "Basic")
+            {
+                givePlayerBasicItem();
+            } else if (whichAnswer == "Premium")
+            {
+                givePlayerPremiumItem();
+            } else { return;  }
         }
 
-        private void AddNPC_AfterSave(object sender, EventArgs args)
-        {
+        //private void DeleteNPC_BeforeSave(object sender, EventArgs args)
+        //{
+        //    if (!Context.IsWorldReady) { return; }
+
+        //    //if (isNPCLoaded == true)
+        //    //{
+        //    //    Game1.removeThisCharacterFromAllLocations(tokenNPC);
+        //    //    isNPCLoaded = false;
+        //    //    foreach (GameLocation location in Game1.locations)
+        //    //    {
+        //    //        NPC[] matches = location.characters.Where(p => p.Name == "TokenMachine").ToArray();
+        //    //        if (matches.Any())
+        //    //            Monitor.Log($"Found the NPC in {location.Name}.", LogLevel.Warn);
+
+        //    //    }
+        //    //}
+        //    //else { return; }
+        //}
+
+        //private void AddNPC_AfterSave(object sender, EventArgs args)
+        //{
             //Texture2D portrait = Helper.Content.Load<Texture2D>("assets/portrait.png", ContentSource.ModFolder);
 
             //if (isNPCLoaded == false)
@@ -238,7 +280,7 @@ namespace PrairieKingPrizes
             //    isNPCLoaded = true;
             //}
             //else { return; }
-        }
+        //}
 
         private void GameEvents_UpdateTick(object sender, EventArgs e)
         {
@@ -265,31 +307,31 @@ namespace PrairieKingPrizes
                 coinsCollected = 0;
             }
 
-            int basicItemID = 444;
-            int premiumItemID = 445;
-            int cancelID = 446;
+            //int basicItemID = 444;
+            //int premiumItemID = 445;
+            //int cancelID = 446;
 
-            foreach (int num in Game1.player.dialogueQuestionsAnswered)
-            {
-                if (num == basicItemID)
-                {
-                    givePlayerBasicItem();
-                    Game1.player.dialogueQuestionsAnswered.Remove(basicItemID);
-                    this.Monitor.Log($"{basicItemID}/basicItemID was a valid dialogue answer and has been removed.");
-                }
-                if (num == premiumItemID)
-                {
-                    givePlayerPremiumItem();
-                    Game1.player.dialogueQuestionsAnswered.Remove(premiumItemID);
-                    this.Monitor.Log($"{premiumItemID}/premiumItemID was a valid dialogue answer and has been removed.");
-                }
-                if (num == cancelID)
-                {
-                    Game1.player.dialogueQuestionsAnswered.Remove(cancelID);
-                    this.Monitor.Log($"{cancelID}/cancelID was a valid dialogue answer and has been removed.");
-                    return;
-                }
-            }
+            //foreach (int num in Game1.player.dialogueQuestionsAnswered)
+            //{
+            //    if (num == basicItemID)
+            //    {
+            //        givePlayerBasicItem();
+            //        Game1.player.dialogueQuestionsAnswered.Remove(basicItemID);
+            //        this.Monitor.Log($"{basicItemID}/basicItemID was a valid dialogue answer and has been removed.");
+            //    }
+            //    if (num == premiumItemID)
+            //    {
+            //        givePlayerPremiumItem();
+            //        Game1.player.dialogueQuestionsAnswered.Remove(premiumItemID);
+            //        this.Monitor.Log($"{premiumItemID}/premiumItemID was a valid dialogue answer and has been removed.");
+            //    }
+            //    if (num == cancelID)
+            //    {
+            //        Game1.player.dialogueQuestionsAnswered.Remove(cancelID);
+            //        this.Monitor.Log($"{cancelID}/cancelID was a valid dialogue answer and has been removed.");
+            //        return;
+            //    }
+            //}
         }
 
         private void givePlayerBasicItem()
@@ -305,6 +347,11 @@ namespace PrairieKingPrizes
 
             if (totalTokens >= 10)
             {
+                totalTokens -= 10;
+                var savedData = this.Helper.ReadJsonFile<SavedData>($"data/{Constants.SaveFolderName}.json") ?? new SavedData();
+                savedData.totalTokens = totalTokens;
+
+
                 if (diceRoll <= 0.01)
                 {
                     //give legendary item
@@ -343,10 +390,6 @@ namespace PrairieKingPrizes
                     this.Monitor.Log($"Attempting to give player an item with the ID of {common[r]}.");
                     Game1.player.addItemByMenuIfNecessary((Item)new StardewValley.Object(common[r], 1, false, -1, 0));
                 }
-                totalTokens -= 10;
-                var savedData = this.Helper.ReadJsonFile<SavedData>($"data/{Constants.SaveFolderName}.json") ?? new SavedData();
-                savedData.totalTokens = totalTokens;
-                Game1.addHUDMessage(new HUDMessage($"Your current Token balance is now {totalTokens}.", 2));
             }
             else
             {
@@ -366,6 +409,11 @@ namespace PrairieKingPrizes
 
             Random random = new Random();
             double diceRoll = random.NextDouble();
+
+            totalTokens -= 50;
+            var savedData = this.Helper.ReadJsonFile<SavedData>($"data/{Constants.SaveFolderName}.json") ?? new SavedData();
+            savedData.totalTokens = totalTokens;
+            Game1.addHUDMessage(new HUDMessage($"Your current Token balance is now {totalTokens}.", 2));
 
             if (totalTokens >= 50)
             {
@@ -407,14 +455,11 @@ namespace PrairieKingPrizes
                     this.Monitor.Log($"Attempting to give player an item with the ID of {common[r]}.");
                     Game1.player.addItemByMenuIfNecessary((Item)new StardewValley.Object(common[r], 25, false, -1, 0));
                 }
-                totalTokens -= 50;
-                var savedData = this.Helper.ReadJsonFile<SavedData>($"data/{Constants.SaveFolderName}.json") ?? new SavedData();
-                savedData.totalTokens = totalTokens;
             }
             else
             {
-                Game1.addHUDMessage(new HUDMessage($"You do not have enough Tokens.", 3));
                 Game1.addHUDMessage(new HUDMessage($"Your current Token balance is {totalTokens}.", 2));
+                Game1.addHUDMessage(new HUDMessage($"You do not have enough Tokens.", 3));
                 return;
             }
         }
